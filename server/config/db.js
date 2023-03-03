33f45c84c7e4935b8a13020e3ae6bfc27504f82b
zerfreeze <yormeno@gmail.com>
2023-03-02 22:19:49 -0500

    connection to mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
  {
- "URI":"mongodb://127.0.0.1:27017/book_store"
+ //"URI":"mongodb://127.0.0.1:27017/book_store"
+ "URI":"mongodb+srv://rormenol:uXR8I7ARASwDGaiz@cluster0.kcyopy1.mongodb.net/?retryWrites=true&w=majority"
 }
